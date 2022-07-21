9656945e6f61d3e4f799608b247a64915d51c714
JJ Garzella <jjgarzella@gmail.com>
2022-07-21 11:54:56 -0700

    Fix the sample code

--- src/sample_code/right_adjoint_preserves_binary_products.lean
+++ src/sample_code/right_adjoint_preserves_binary_products.lean
@@ -3,7 +3,12 @@ import category_theory.limits.has_limits
 import category_theory.adjunction.basic
 import category_theory.closed.cartesian
 
+
+set_option pp.universes true
+
 noncomputable theory
+
+
 
 universes u v
 
@@ -16,13 +21,14 @@ variables {D : Type u} [category.{v} D] [has_limits D]
 variables (F : C ⥤ D) (G : D ⥤ C) [adj : F ⊣ G]
 include adj
 
-lemma RAPL_binary_products (a b : D) : G.obj (a ⨯ b) ≅ (G.obj a) ⨯ (G.obj b) 
-:= 
+variables (x y : D)
+
+lemma RAPL_binary_products (a b : D) : G.obj (a ⨯ b) ≅ (G.obj a) ⨯ (G.obj b)
+:=
 begin
-  have pres : preserves_limits_of_size G,
+  haveI pres_all : preserves_limits_of_size.{0 0} G,
     by exact right_adjoint_preserves_limits adj,
-  apply_instance,
-  -- What goes here? 
-  -- Pretty sure I should not be unfolding the definition into limit cones
+  apply preserves_limit_pair.iso
+  -- exact preserves_limit_pair.iso G a b
 end
 
