7d223248ad2d40d81ad1681db72eced40ddf0210
Calvin Lee <calvins.lee@utah.edu>
2023-01-28 11:37:56 +0800

    add kähler localization theorem

--- src/kaehler.lean
+++ src/kaehler.lean
@@ -1,24 +1,34 @@
 import ring_theory.derivation
 import algebra.module.localized_module
 import ring_theory.localization.basic
-import algebra.algebra.basic
-
-section localization
-variables {R A SR : Type*} [comm_ring R] [comm_ring A]  [algebra R A]
-variables  (S : submonoid R) 
-
-instance : algebra.is_pushout R (localization S) A (localized_module S A) := sorry
-
-end localization
 
 section kaehler
 noncomputable theory
-variables {A B : Type*} [comm_ring A] [comm_ring B] [algebra B A] {S : submonoid B}
+variables (A B : Type*) [comm_ring A] [comm_ring B] [algebra B A] (S : submonoid B)
 
-#check Ω[A⁄B]
+def localized_module.algebra_hom : A →ₐ[B] localized_module S A :=
+ alg_hom.of_linear_map (localized_module.mk_linear_map S A)
+ begin
+  rw [localized_module.mk_linear_map_apply],
+  refl,
+end
+begin
+  intros,
+  iterate 3 { rw [localized_module.mk_linear_map_apply] },
+  rw [localized_module.mk_mul_mk, one_mul],
+end
 
---#check Ω[localizat⁄SB']
+instance localized_module.algebra'' : algebra A (localized_module S A) :=
+  ring_hom.to_algebra (alg_hom.to_ring_hom (localized_module.algebra_hom A B S))
 
+instance localized_module.is_scalar_tower' : is_scalar_tower B A (localized_module S A) :=
+{ smul_assoc := by simp only [smul_assoc, eq_self_iff_true, forall_const] }
+instance localized_module.is_pushout :
+    algebra.is_pushout B (localization S) A (localized_module S A) :=
+begin
+  rw algebra.is_pushout_iff,
+  convert is_localized_module.is_base_change S (localized_module.mk_linear_map S A),
+end
 
 /-
  - Outline:
@@ -29,12 +39,17 @@ variables {A B : Type*} [comm_ring A] [comm_ring B] [algebra B A] {S : submonoid
  - ((localization S) ⊗[B] Ω[A⁄B]) ≃ₗ[B] Ω[(localized_module S A)⁄(localization S)]
  - but we need `is_pushout R (localization S) A (localized_module S A)` to call this
  -/
-theorem kaehler_commutes_wih_localization : (localized_module S Ω[A⁄B]) ≃ₗ[B] Ω[localized_module S A ⁄localization S] :=
+theorem kaehler_commutes_wih_localization :
+    (localized_module S Ω[A⁄B]) ≃ₗ[B] Ω[localized_module S A ⁄localization S] :=
 begin
-  admit
+  have tprod_eq_rhs := kaehler_differential.derivation_tensor_product_equiv B (localization S) A
+      (localized_module S A),
+  suffices lhs_eq_tprod :
+      (localized_module S Ω[A⁄B]) ≃ₗ[localization S] (tensor_product B (localization S) Ω[A ⁄ B]),
+  {
+    exact linear_equiv.restrict_scalars B (linear_equiv.trans lhs_eq_tprod tprod_eq_rhs),
+  },
+  have := is_localized_module.is_base_change S (localized_module.mk_linear_map S Ω[A⁄B]),
+  exact (is_base_change.equiv this).symm,
 end
-
-
 end kaehler
-#check kaehler_differential.derivation_tensor_product_equiv
-
